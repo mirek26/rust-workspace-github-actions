1c70a4f93efd8792ac557a540ad96ad714a78703
Mirek Klimos <m.klimos@lithic.com>
2023-03-08 17:39:14 -0800

    fixx

--- .github/detect_affected.py
+++ .github/detect_affected.py
@@ -6,11 +6,11 @@ import sys
 metadata = json.loads(subprocess.check_output(['cargo', 'metadata']).decode("utf-8"))
 packages = [p['name'] for p in metadata['packages']]
 deps = {p['name']: [dep['name'] for dep in p['dependencies']] for p in metadata['packages']}
-print(f"::debug::all-packages={packages}", file=sys.stderr)
+print(f"::debug::all-packages={packages}")
 
 # Get changed files
 changed_files = subprocess.check_output(["git", "diff", "--name-only", sys.argv[1], sys.argv[2]]).decode("utf-8").split()
-print(f"::debug::changed-files={packages}", file=sys.stderr)
+print(f"::debug::changed-files={packages}")
 
 # Calculate transitive dependencies
 affected = [p for p in packages if any(file.startswith(p + '/') for file in changed_files)]
@@ -19,4 +19,4 @@ while True:
     if len(new_affected) == len(affected):
         break
     affected = new_affected
-print(f"{affected}={json.dumps(affected)}")
+print(f"{affected}={json.dumps(affected)}", file=sys.stderr)
